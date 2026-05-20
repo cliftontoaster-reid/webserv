94f2c7cc16ec694cd5ffeeec15da352b63ab301e
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2026-05-20 16:58:28 +0200

    Removed custon exceptions.

--- libs/toml98/include/Exception.hpp
+++ libs/toml98/include/Exception.hpp
@@ -2,20 +2,9 @@
 #ifndef TOML98_INCLUDE_EXCEPTION_HPP
 #define TOML98_INCLUDE_EXCEPTION_HPP
 
-#include <exception>
-
 #define STRINGIZE(x) #x
 #define TODO()                                                  \
   throw std::runtime_error("TODO: not implemented at " __FILE__ \
                            ":" STRINGIZE(__LINE__))
-
-namespace toml98 {
-
-class EarlyEndOfFile : public std::exception {
- public:
-  const char* what() const throw() { return "Early end of file"; }
-};
-
-};  // namespace toml98
 
 #endif
