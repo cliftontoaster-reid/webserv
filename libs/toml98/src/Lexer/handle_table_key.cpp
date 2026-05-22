4467ec8d7c1c659aa9fe7e4884bcd1dba2ae7d0a
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2026-05-22 13:41:15 +0200

    feat(Lexer): Implement handle_table_key() function.

--- libs/toml98/src/Lexer/handle_table_key.cpp
+++ libs/toml98/src/Lexer/handle_table_key.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <stdexcept>
 
 #include "Exception.hpp"
@@ -6,6 +7,61 @@
 namespace toml98 {
 
 // NOLINTNEXTLINE
-Token* Lexer::handle_table_key() { TODO(); }
+Token* Lexer::handle_table_key() {
+  while (true) {
+    char now = peek();
+
+    switch (now) {
+      case '\0':
+        throw std::runtime_error("Early end of file reading table key.");
+
+      case ' ':
+      case '\t':
+        _stack.push(LexerWhiteSpace);
+        return NULL;
+
+      case '.':
+        pop();
+        return new Token(TokenDot, ".");
+
+      case ']':
+        _stack.pop();
+        pop();
+        return new Token(TokenTableEnd, "]");
+
+      case '"':
+      case '\'': {
+        char quote = now;
+        pop();
+
+        if (canPeek(quote) && canPeek(quote, 1)) {
+          throw std::runtime_error(
+              "Table keys cannot contain multi-line strings.");
+        }
+        if (canPeek()) {
+          _stack.push(quote == '"' ? LexerStringDouble : LexerString);
+        } else {
+          throw std::runtime_error("Unterminated string: Early end of file.");
+        }
+        break;
+      }
+
+      case '-':
+      case '_':  // A-Za-z0-9_- (see alphanum above)
+        _stack.push(LexerWord);
+        return NULL;
+
+      default: {
+        if (std::isalnum(now) != 0) {
+          _stack.push(LexerWord);
+          return NULL;
+        }
+
+        throw std::runtime_error(
+            "Unterminated literal string: Unknown character in Normal");
+      }
+    }
+  }
+}
 
 }  // namespace toml98
